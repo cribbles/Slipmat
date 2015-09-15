2bba140db82837ea4d895ec70fdd4f70c58a7411
cribbles <yaphetf@gmail.com>
2015-09-15 15:53:47 -0400

    Add Artist and Label API endpoints

---
+++ app/controllers/api/artists_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  class ArtistsController < ApplicationController
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      render json: @artist
+    else
+      render json: @artist.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+
+    if @artist.update(artist_params)
+      render json: @artist
+    else
+      render json @artist.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy!
+
+    render json: @artist
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+
+    render json: @artist
+  end
+
+  def index
+    @artist = Artist.all
+
+    render json: @artist
+  end
+
+  private
+
+  def artist_params
+    params
+      .require(:artist)
+      .permit(:name, :real_name, :profile)
+  end
+end
---
+++ app/controllers/api/labels_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  class LabelsController < ApplicationController
+
+  def create
+    @label = Label.new(label_params)
+
+    if @label.save
+      render json: @label
+    else
+      render json: @label.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @label = Label.find(params[:id])
+
+    if @label.update(label_params)
+      render json: @label
+    else
+      render json @label.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @label = Label.find(params[:id])
+    @label.destroy!
+
+    render json: @label
+  end
+
+  def show
+    @label = Label.find(params[:id])
+
+    render json: @label
+  end
+
+  def index
+    @label = Label.all
+
+    render json: @label
+  end
+
+  private
+
+  def label_params
+    params
+      .require(:label)
+      .permit(:title, :profile)
+  end
+end
