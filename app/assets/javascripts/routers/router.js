5b53fdbce770c7e22ed6c57fc7f195e9afbae88a
cribbles <yaphetf@gmail.com>
2015-09-16 11:22:26 -0400

    Add Backbone edit/show/index Record routes

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -5,11 +5,38 @@ Slipmat.Routers.Router = Backbone.Router.extend({
   },
 
   routes: {
-    "records/new": "recordNew"
+    "records": "recordIndex",
+    "records/new": "recordNew",
+    "records/:id": "recordShow",
+    "records/:id/edit": "recordEdit"
+  },
+
+  recordIndex: function () {
+    var records = new Slipmat.Collections.Records();
+    records.fetch();
+    var view = new Slipmat.Views.RecordIndex({ collection: records });
+
+    this._swapView(view);
   },
 
   recordNew: function () {
     var record = new Slipmat.Models.Record();
+    var view = new Slipmat.Views.RecordForm({ model: record });
+
+    this._swapView(view);
+  },
+
+  recordShow: function (id) {
+    var record = new Slipmat.Models.Record({ id: id });
+    record.fetch();
+    var view = new Slipmat.Views.RecordNew({ model: record });
+
+    this._swapView(view);
+  },
+
+  recordEdit: function (id) {
+    var record = new Slipmat.Models.Record({ id: id });
+    record.fetch();
     var view = new Slipmat.Views.RecordForm({ model: record });
 
     this._swapView(view);
