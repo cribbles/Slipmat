efb9d9115b943f60115a63c073c8a90964870461
cribbles <yaphetf@gmail.com>
2015-09-16 15:58:56 -0400

    Import CompositeView

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 //= require backbone
 //= require slipmat
 //= require_tree ../templates
+//= require_tree ./utils
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
