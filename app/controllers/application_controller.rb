47d295196be1d0833347b93b7317fe27f138be1f
cribbles <yaphetf@gmail.com>
2015-09-17 09:16:03 -0400

    Refactor User auth helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  helper_method :current_user
-  helper_method :signed_in?
+  protect_from_forgery with: :null_session
+  helper_method :current_user, :signed_in?
 
   def sign_in!(user)
     session[:session_token] = user.reset_session_token!
     @current_user = user
   end
 
-  def sign_out!(user)
-    user.try(:reset_session_token!)
+  def sign_out!
+    current_user.try(:reset_session_token!)
     session[:session_token] = nil
   end
 
@@ -21,9 +20,5 @@ class ApplicationController < ActionController::Base
 
   def signed_in?
     !!current_user
-  end
-
-  def ensure_signed_out
-    redirect_to root_url if signed_in?
   end
 end
