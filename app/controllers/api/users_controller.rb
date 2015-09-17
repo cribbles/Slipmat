fbac1811491e6a309f844dadf60fc71b4eafe4e7
cribbles <yaphetf@gmail.com>
2015-09-17 09:06:27 -0400

    Add Api::UsersController

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  class UsersController < ApplicationController
+
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        sign_in!(@user)
+        render :show
+      else
+        render json: @user.errors.full_messages, status: 422
+      end
+    end
+
+    private
+
+    def user_params
+      params
+        .require(:user)
+        .permit(:username, :email, :password)
+    end
+  end
+end
