72faf2c8acbaf965c31060d7644013c721c73f2c
cribbles <yaphetf@gmail.com>
2015-09-18 11:45:07 -0400

    Remove profile header event handler upon UserShow removal

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -13,22 +13,24 @@ Slipmat.Views.UserShow = Backbone.CompositeView.extend({
   },
 
   render: function () {
-    $(".profile-header").remove();
-
     var header = this.headerTemplate({ user: this.model });
     var content = this.template({ user: this.model });
     var profile = JST["users/_profile"]({ user: this.model });
 
     this.$rootEl.before(header);
     this.$el.html(content);
-    this.$(".profile-main").html(profile);
 
+    $(".profile-header").remove();
+    this.$(".profile-main").html(profile);
     $(".tab").on("click", this.switchTab.bind(this));
+
     return this;
   },
 
   remove: function () {
     $(".profile-header").remove();
+    $(".tab").off("click");
+
     Backbone.View.prototype.remove.call(this);
   },
 
