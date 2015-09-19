6b28c4c0f1f6ed4e5b610d4db1ac51780a365e21
cribbles <yaphetf@gmail.com>
2015-09-19 17:14:49 -0400

    Don't validate track's record before saving(fixes rollback issue)

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :record
-  validates :title, :record, :ord, presence: true
+  validates :title, :ord, presence: true
   before_validation :ensure_ord
 
   default_scope { order(:ord) }
@@ -9,8 +9,15 @@ class Track < ActiveRecord::Base
 
   def ensure_ord
     if ord.blank?
-      highest_previous_ord = record.tracks.max_by(&:ord).try(:ord) || 0
+      if record && record.tracks.any?
+        highest_previous_ord = record.tracks.max_by(&:ord).ord
+      else
+        highest_previous_ord = 0
+      end
+
       self.ord = highest_previous_ord + 1
     end
+
+    puts self.attributes
   end
 end
