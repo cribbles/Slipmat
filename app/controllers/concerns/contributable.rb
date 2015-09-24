c46fc2c9f8105b93305d2b1a4ef7f5140a08df2b
cribbles <yaphetf@gmail.com>
2015-09-24 07:51:41 -0400

    Don't attempt to add UserContribution if one already exists

--- app/controllers/concerns/contributable.rb
+++ app/controllers/concerns/contributable.rb
@@ -2,10 +2,12 @@ module Contributable
   extend ActiveSupport::Concern
 
   def add_contribution(model)
-    UserContribution.create(
+    params = {
       user_id: current_user.id,
       contributable_id: model.id,
-      contributable_type: model.class
-    )
+      contributable_type: model.class.to_s
+    }
+
+    UserContribution.create(params) unless UserContribution.find_by(params)
   end
 end
