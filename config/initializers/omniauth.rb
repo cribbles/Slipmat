12f3caecc3043a2c76835a7511c752596b34b70b
cribbles <yaphetf@gmail.com>
2015-09-24 14:55:43 -0400

    Add secure_image_url param in Facebook omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-	         image_size: { width: 150, height: 150 }
+	         image_size: { width: 150, height: 150 },
+					 secure_image_url: true
 end
