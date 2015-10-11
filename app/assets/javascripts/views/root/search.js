d5fda8d9b4df836377407f2e15b18e921c41ee0c
cribbles <yaphetf@gmail.com>
2015-10-11 13:01:35 -0400

    Extend view for null search results

--- app/assets/javascripts/views/root/search.js
+++ app/assets/javascripts/views/root/search.js
@@ -8,12 +8,22 @@ Slipmat.Views.Search = Backbone.PaginatableView.extend({
     this.listenTo(this.collection, "sync", this.render);
   },
 
+  events: {
+    "click button": "redirect"
+  },
+
   render: function () {
     var content = this.template();
     this.$el.html(content);
     this.$("h2").text("Search Results");
-    this.renderCollection();
+    this.renderPages();
 
+    if (this.collection.length) {
+      this.renderSubviews();
+    } else {
+      var content = JST["root/_nullSearchResults"]();
+      this.$(".content-records").html(content);
+    }
     return this;
   },
 
@@ -25,6 +35,10 @@ Slipmat.Views.Search = Backbone.PaginatableView.extend({
       var content = model.subview({ model: model });
       view.$(".content-records").append(content);
     });
+  },
+
+  redirect: function () {
+    Backbone.history.navigate("records/new", { trigger: true });
   }
 
 });
